daa4e72101f3da0043907cbdfd086757be186d1c
Tushar Deshpande <tdeshpande.97@gmail.com>
2021-02-21 01:33:24 +0530

    Add files via upload

---
+++ Domino_piling_50_A.cpp
@@ -0,0 +1,19 @@
+/*
+This is a codeforces problem. 
+Link to the problem: https://codeforces.com/problemset/problem/50/A
+*/
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int M,N,count=0;
+
+    cin>>M>>N;
+
+    count=(M*N)/2;
+
+    cout<<count;
+
+    return 0;
+}
